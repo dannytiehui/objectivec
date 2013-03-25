09effa703f3e19f059f253c3eadfef864dbbe622
Tiehui.Lu <dannytiehui@hotmail.com>
2013-03-25 11:52:06 +0800

    提交到本地

---
+++ ObjectiveC/NSObject+prog19.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+prog19.h
+//  ObjectiveC
+//
+//  Created by Tiehui.Lu on 13-3-25.
+//  Copyright (c) 2013年 zhnews. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (prog19)
+
+@end
---
+++ ObjectiveC/NSObject+prog19.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+prog19.m
+//  ObjectiveC
+//
+//  Created by Tiehui.Lu on 13-3-25.
+//  Copyright (c) 2013年 zhnews. All rights reserved.
+//
+
+#import "NSObject+prog19.h"
+
+@implementation NSObject (prog19)
+
+@end
