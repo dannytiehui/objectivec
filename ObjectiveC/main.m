91143775fd2578e10c6c1b65101386e5ab8e8032
dannytiehui <dannytiehui@hotmail.com>
2013-03-25 10:48:35 +0800

    Initial Commit

---
+++ ObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectiveC
+//
+//  Created by Tiehui.Lu on 13-3-25.
+//  Copyright (c) 2013年 zhnews. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
